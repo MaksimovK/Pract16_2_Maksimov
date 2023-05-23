d6c0ec8620185dbac806153b5c97ceda46b2c49c
MaksimovK <anderjblinov35@gmail.com>
2023-05-23 19:05:46 +0500

    New commit

---
+++ Pract16_Maksimov/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Pract16_Maksimov
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите слово для поиска: ");
+            string searchWord = Console.ReadLine();
+            string filePath = "File.txt";
+            try
+            {
+                if( new FileInfo( "File.txt" ).Length == 0 )
+                {
+                    throw new Exception("Файл пуст");
+                }
+                string text = File.ReadAllText(filePath);
+
+                int count = CountOccurrences(text, searchWord, StringComparison.OrdinalIgnoreCase);
+
+                Console.WriteLine($"Было найдено {count} вхождений (ий) поискового запроса \"{searchWord}\".");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+        }
+
+        static int CountOccurrences(string text, string searchWord, StringComparison comparison)
+        {
+            int count = 0;
+            int index = 0;
+
+            while ((index = text.IndexOf(searchWord, index, comparison)) != -1)
+            {
+                count++;
+                index += searchWord.Length;
+            }
+
+            return count;
+        }
+    }
+}
